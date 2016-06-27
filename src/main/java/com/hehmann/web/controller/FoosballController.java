4eb58111c32a7f99206986c4f7d5e69edb95f418
th2 <github2@hehmann.com>
2016-06-27 11:14:21 +0200

    send API response in JSON format

--- src/main/java/com/hehmann/web/controller/FoosballController.java
+++ src/main/java/com/hehmann/web/controller/FoosballController.java
@@ -2,11 +2,13 @@ package com.hehmann.web.controller;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -41,10 +43,8 @@ public class FoosballController {
 	public String showTournamentDetails(Map<String, Object> model, HttpServletRequest request) {
 		Integer tournamentId;
 		try {
-			tournamentId = Integer.parseInt(URLDecoder.decode(request.getRequestURI().substring(request.getRequestURI().lastIndexOf('/') + 1), "iso-8859-1"));
-		} catch (UnsupportedEncodingException e) {
-			model.put("message", "Ungültige TurnierId.");
-			return "tournamentError";
+			String[] parameters = request.getRequestURI().split("/");
+			tournamentId = Integer.parseInt(parameters[2]);
 		} catch (NumberFormatException e) {
 			model.put("message", "Ungültige TurnierId.");
 			return "tournamentError";
@@ -57,6 +57,50 @@ public class FoosballController {
 		
 		model.put("tournament", tournaments.get(tournamentId));
 		return "tournamentView";
+	}
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/**", method = RequestMethod.GET)
+	public String getTournamentData(Map<String, Object> model, HttpServletRequest request) {
+		Integer tournamentId;
+		String action;
+		
+		try {
+			String[] parameters = request.getRequestURI().split("/");
+			tournamentId = Integer.parseInt(parameters[2]);
+			action = URLDecoder.decode(parameters[3], "iso-8859-1");
+		} catch (NumberFormatException e) {
+			return sendData(model, getDataError(1)); // invalid tournamentId
+		} catch (UnsupportedEncodingException e) {
+			return sendData(model, getDataError(2)); // invalid action
+		}
+		
+		if(!tournaments.containsKey(tournamentId)) {
+			return sendData(model, getDataError(0)); // tournamentId not found
+		}
+
+		return sendData(model, getDataResponse(tournamentId, action));
+	}
+	
+	@SuppressWarnings("unchecked")
+	private JSONObject getDataResponse(Integer tournamentId, String action) {
+		JSONObject response = new JSONObject();
+		response.put("tournamentId", tournamentId);
+		response.put("action", action);
+		response.put("tournamentName", tournaments.get(tournamentId).getName());
+		return response;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private JSONObject getDataError(Integer errorCode) {
+		JSONObject response = new JSONObject();
+		response.put("error", errorCode);
+		return response;
+	}
+
+	private String sendData(Map<String, Object> model, JSONObject response) {
+		model.put("content", response);
+		return "data";
 	}
 	
 	@RequestMapping(value = "/data", method = RequestMethod.GET)
